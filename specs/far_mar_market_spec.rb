2452ee2ee14a7e5abaf6364e7c0d0e60dc923d15
Erin Bond <erin@erinebond.com>
2016-09-06 11:29:30 -0700

    Wrote test to ensure Market objects can be made

--- specs/far_mar_market_spec.rb
+++ specs/far_mar_market_spec.rb
@@ -3,4 +3,10 @@ require_relative "../lib/far_mar_market"
 
 
 describe "Testing FarMar Market" do
+
+    it "Confirm that new Market objects can be created" do
+      market = FarMar::Market.new
+      expect(market.class.must_equal(FarMar::Market))
+    end
+
 end
