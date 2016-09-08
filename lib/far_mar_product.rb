eb2d17ead0ec1430d54889f0eb1a1ebb845044c8
Erin Bond <erin@erinebond.com>
2016-09-08 13:11:18 -0700

    Added product.sales method, product.number_of_sales_method, product.by_vendor method and corresponding tests

--- lib/far_mar_product.rb
+++ lib/far_mar_product.rb
@@ -51,10 +51,32 @@ class FarMar::Product
   end#of method
 
 #sales: returns a collection of FarMar::Sale instances that are associated using the FarMar::Sale product_id field.
+  def sales
+    sales_of_this_product = []
+    sales = FarMar::Sale.all
+    sales.each do |sale|
+      if sale.product_id == self.id
+        sales_of_this_product << sale
+        end#of if
+      end#of do
+    return sales_of_this_product
+  end#of method
 
 #number_of_sales: returns the number of times this product has been sold.
+  def number_of_sales
+    return sales.length
+  end#of method
 
 #self.by_vendor(vendor_id): returns all of the products with the given vendor_id
+  def self.by_vendor(vendor_id)
+    products_with_this_vendor_id = []
+      @products.each do |product|
+      if product.vendor_id == vendor_id
+        products_with_this_vendor_id << product
+      end
+    end
+    return products_with_this_vendor_id
+  end
 
 end#of class
 
--- specs/far_mar_product_spec.rb
+++ specs/far_mar_product_spec.rb
@@ -25,17 +25,36 @@ describe "Testing FarMar Product" do
       #is it returning a product?
       expect(FarMar::Product.find(8178).class).must_equal(FarMar::Product)
       #is it the correct product?
-
-      
+      expect(FarMar::Product.find(8178).id).must_equal(8178)
     end
 
     it ".vendor returns the vendor object for the vendor that sells that product" do
       #is it returning a vendor?
       expect(product.vendor.class).must_equal(FarMar::Vendor)
       #is it returning the correct vendor?
-
+      expect(product.vendor.id).must_equal(product.vendor_id)
     end
 
+    it ".sales returns a collection of sales instances that are associated with this product" do
+        #is it returning a sales objects?
+        expect(product.sales.class).must_equal(Array)
+        expect(product.sales.sample.class).must_equal(FarMar::Sale)
+        #is it returning the correct sales objects
+        expect(product.sales.sample.product_id).must_equal(product.id)
+    end
+
+    it ".number_of_sales returns the number of sales associated with this product" do
+      #is it returning a number?
+      expect(product.number_of_sales.class).must_equal(Fixnum)
+      #is it returning the correct number?
+      expect(product.number_of_sales).must_equal(4)
+    end
+
+    it "self.by_vendor returns all of the vendors with the given product_id" do
+      products
+      expect(FarMar::Product.by_vendor(2612).class).must_equal(Array)
+      expect(FarMar::Product.by_vendor(2612)[0].class).must_equal(FarMar::Product)
+    end
 
 
 end
